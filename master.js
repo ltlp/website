6b17f2f04b0bdbda625a6125aacd778a875f061b
ltlp <42503753+ltlp@users.noreply.github.com>
2020-01-08 21:46:13 -0700

    Added functions

--- master.js
+++ master.js
@@ -15,3 +15,53 @@ function closeFlaskInfo() {
 	document.getElementById("flaskInfo").style.width = "0";
 	document.getElementById("main").style.marginRight = "0";
 };
+
+function openWriterInfo() {
+	document.getElementById("writerInfo").style.width = "65%";
+	document.getElementById("main").style.marginRight = "65%";
+};
+
+function closeWriterInfo() {
+	document.getElementById("writerInfo").style.width = "0";
+	document.getElementById("main").style.marginRight = "0";
+};
+
+function openWebInfo() {
+	document.getElementById("webInfo").style.width = "65%";
+	document.getElementById("main").style.marginRight = "65%";
+};
+
+function closeWebInfo() {
+	document.getElementById("webInfo").style.width = "0";
+	document.getElementById("main").style.marginRight = "0";
+};
+
+function openKpiInfo() {
+	document.getElementById("kpiInfo").style.width = "65%";
+	document.getElementById("main").style.marginRight = "65%";
+};
+
+function closeKpiInfo() {
+	document.getElementById("kpiInfo").style.width = "0";
+	document.getElementById("main").style.marginRight = "0";
+};
+
+function openPstrmiInfo() {
+	document.getElementById("pstrmiInfo").style.width = "65%";
+	document.getElementById("main").style.marginRight = "65%";
+};
+
+function closePstrmiInfo() {
+	document.getElementById("pstrmiInfo").style.width = "0";
+	document.getElementById("main").style.marginRight = "0";
+};
+
+function openDataInfo() {
+	document.getElementById("dataInfo").style.width = "65%";
+	document.getElementById("main").style.marginRight = "65%";
+};
+
+function closeDataInfo() {
+	document.getElementById("dataInfo").style.width = "0";
+	document.getElementById("main").style.marginRight = "0";
+};
