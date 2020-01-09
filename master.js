95a35183e92806e4f2ba6cb018d7e5fe752ae9d0
ltlp <42503753+ltlp@users.noreply.github.com>
2020-01-08 21:29:01 -0700

    Added functions

--- master.js
+++ master.js
@@ -1,7 +1,17 @@
 function openSidebar() {
-  document.getElementById("sidebar").style.width = "240px";
+	document.getElementById("sidebar").style.width = "240px";
 };
 
 function closeSidebar() {
-  document.getElementById("sidebar").style.width = "0";
+	document.getElementById("sidebar").style.width = "0";
+};
+
+function openFlaskInfo() {
+	document.getElementById("flaskInfo").style.width = "65%";
+	document.getElementById("main").style.marginRight = "65%";
+};
+
+function closeFlaskInfo() {
+	document.getElementById("flaskInfo").style.width = "0";
+	document.getElementById("main").style.marginRight = "0";
 };
